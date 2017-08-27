b957d4aea381540edebd3a726f5176fa91b49649
Mohtshum <mohtisham.zubair@crossover.com>
2017-08-27 23:38:06 +0500

    Schedulingdemo

---
+++ Platform/DNN.Scheduling/ScheduleLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DNN.Scheduling
+{
+    internal class ScheduleLog
+    {
+        public ScheduleLog()
+        {
+        }
+
+        public string Msg { get; set; }
+        public DateTime OnDate { get; set; }
+        public int ScheduleID { get; set; }
+    }
+}
--- Platform/DNN.Scheduling/SchedulingUtility.cs
+++ Platform/DNN.Scheduling/SchedulingUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DotNetNuke.Data;
 
 namespace DNN.Scheduling
 {
@@ -6,7 +7,12 @@ namespace DNN.Scheduling
     {
         internal static void WriteLog(DateTime currentTime)
         {
-            
+            DataProvider dbHandler = DataProvider.Instance();
+            dbHandler.ExecuteReader("DotSee_Scheduling", null);
+
+            // dbhandler must do these
+            var logOfSchedule =  new ScheduleLog() { ScheduleID = 1, Msg = "some log msg", OnDate = currentTime };
+            // dbHandler.Save(logOfSchedule);
         }
     }
 }
