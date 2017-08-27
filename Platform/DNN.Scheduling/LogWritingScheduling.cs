1510d406f9f25b8899671903496d41052e3eec1d
Mohtshum <mohtisham.zubair@crossover.com>
2017-08-27 23:23:52 +0500

    Schedulingdemo
    Only a copypaste Example to user intenal of DNN for
    - Scheduling
    - DataProvider
    - logging

---
+++ Platform/DNN.Scheduling/LogWritingScheduling.cs
@@ -0,0 +1,45 @@
+﻿using DotNetNuke.Services.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNN.Scheduling
+{
+    public class LogWritingScheduling : SchedulerClient
+    {
+        private ScheduleHistoryItem shItem;
+
+        public LogWritingScheduling(ScheduleHistoryItem shItem) : base()
+        {
+            this.shItem = shItem;
+        }
+
+        public override void DoWork()
+        {
+            bool schedulingDone = true;
+
+            try
+            {
+                SchedulingUtility.WriteLog(DateTime.Now);
+                //To log note                
+                InsertLogNote(" logging done by scheduling");
+                //Show success
+                this.ScheduleHistoryItem.Succeeded = schedulingDone;
+            }
+            catch (Exception ex)
+            {
+                this.ScheduleHistoryItem.Succeeded = !schedulingDone;
+                InsertLogNote("Exception= " + ex.ToString());
+                this.Errored(ref ex);
+                DotNetNuke.Services.Exceptions.Exceptions.LogException(ex);
+            }
+        }
+
+        private void InsertLogNote(string logMessage)
+        {
+            this.ScheduleHistoryItem.AddLogNote(logMessage);
+        }
+    }
+}
---
+++ Platform/DNN.Scheduling/SchedulingUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNN.Scheduling
+{
+    internal class SchedulingUtility
+    {
+        internal static void WriteLog(DateTime currentTime)
+        {
+            
+        }
+    }
+}
